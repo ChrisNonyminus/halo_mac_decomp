549f2bea71a2215c9b8124e8474bb20ecc15f8a0
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-03-13 12:27:44 -0600

    update

--- auto_assemble_tmp_code.sh
+++ auto_assemble_tmp_code.sh
@@ -46,8 +46,8 @@ do
             ${COMPILER_PATH}/${COMPILER} ./tmp/code.cpp -I${COMPILER_PATH}/../powerpc-apple-darwin/include -I${COMPILER_PATH}/../powerpc-apple-darwin/include/c++/4.0.0 ${COMPILER_FLAGS}
         fi
 
-        python3 tools/convert_gas_syntax.py ./tmp/code.s > ./tmp/code_new.s
-        python3 tools/convert_gas_syntax.py ./tmp/orig.s > ./tmp/orig_new.s
+        python3 tools/convert_gas_syntax.py ./tmp/code.s "${FUNCTION}" new > ./tmp/code_new.s
+        python3 tools/convert_gas_syntax.py ./tmp/orig.s "${FUNCTION}" old > ./tmp/orig_new.s
 
         powerpc-linux-gnu-as tmp/code_new.s -o tmp/code_new.o
         powerpc-linux-gnu-as tmp/orig_new.s -o tmp/orig_new.o
@@ -65,12 +65,12 @@ do
         if [[ "$COMPILER" == "cc1plus" ]]; then
             ${COMPILER_PATH}/${COMPILER} ./tmp/code.cpp -I${COMPILER_PATH}/../powerpc-apple-darwin/include -I${COMPILER_PATH}/../powerpc-apple-darwin/include/c++/4.0.0 ${COMPILER_FLAGS}
         fi
-
-        python3 tools/convert_gas_syntax.py ./tmp/code.s > ./tmp/code_new.s
-        python3 tools/convert_gas_syntax.py ./tmp/orig.s > ./tmp/orig_new.s
+        python3 tools/convert_gas_syntax.py ./tmp/code.s "${FUNCTION}" new > ./tmp/code_new.s
+        python3 tools/convert_gas_syntax.py ./tmp/orig.s "${FUNCTION}" old > ./tmp/orig_new.s
 
         powerpc-linux-gnu-as tmp/code_new.s -o tmp/code_new.o
         powerpc-linux-gnu-as tmp/orig_new.s -o tmp/orig_new.o
+        mkdir -p expected/tmp
         cp tmp/orig_new.o expected/tmp/code_new.o
 
         python3 tools/asm_differ/diff.py --format plain -o ${FUNCTION} -f tmp/code_new.o > tmp/diff.txt
